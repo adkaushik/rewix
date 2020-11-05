239ee8ce34b3262fbd25de3408c168397a9476ba
avdkishore <avdkishore@gmail.com>
2020-11-05 13:25:55 +0530

    Custom swiper component

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass')
 const config = require('./config');
 
-module.exports = withPlugins([[withCSS]], {
+module.exports = withPlugins([
+  [withCSS],
+  [withSass, 
+    // {
+    //   cssModules: true,
+    //   cssLoaderOptions: {
+    //     localIdentName: '[path]___[local]___[hash:base64:5]',
+    //   },
+    // }
+  ],
+], {
   assetPrefix: '',
   env: {
     url: config.url,
